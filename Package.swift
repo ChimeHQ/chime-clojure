7d721017e2cf8e5ff128a6658b75fe8694f11f38
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-11-14 13:25:26 -0500

    Fix some copy-pasta

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import PackageDescription
 
 let package = Package(
-	name: "chime-swift",
+	name: "chime-clojure",
 	platforms: [.macOS(.v11)],
 	products: [
 		.library(name: "ChimeClojure", targets: ["ChimeClojure"]),
--- Tests/ChimeClojureTests/ChimeClojureTests.swift
+++ Tests/ChimeClojureTests/ChimeClojureTests.swift
@@ -1,12 +1,7 @@
 import XCTest
-@testable import chime_clojure
+@testable import ChimeClojure
 
-final class chime_clojureTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+final class ChimeClojureTests: XCTestCase {
+	func testExample() throws {
+	}
 }
